b81c246011b47584a8c33ff6dfe88f29a132ff7e
agungnugrahaid <agungnugraha.id@gmail.com>
2026-04-26 07:46:22 +0000

    Adjust Models to Database Structure

--- app/Models/Article.php
+++ app/Models/Article.php
@@ -6,5 +6,23 @@ use Illuminate\Database\Eloquent\Model;
 
 class Article extends Model
 {
-    //
+    protected $fillable = [
+        'title',
+        'slug',
+        'content',
+        'category_id',
+        'featured_image',
+        'is_published',
+        'published_at',
+    ];
+
+    protected $casts = [
+        'is_published' => 'boolean',
+        'published_at' => 'datetime',
+    ];
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
 }
--- app/Models/Page.php
+++ app/Models/Page.php
@@ -6,5 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Page extends Model
 {
-    //
+    protected $fillable = [
+        'title',
+        'slug',
+        'content',
+        'meta_title',
+        'meta_description',
+    ];
 }
